991840c66ecd40d343af1218c36d63b04eb2acb3
Greg Pendleton <gpendleton@gmail.com>
2021-09-18 11:25:51 -0500

    changed breakpoints to groups from recipes

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const cards = document.querySelectorAll(".recipe-card")
+const groups = document.querySelectorAll(".recipe-group")
 const all_btn = document.querySelector('#all')
 const entree_btn = document.querySelector('#entrees')
 const sides_btn = document.querySelector('#sides')
 const breads_btn = document.querySelector('#breads')
 const deserts_btn = document.querySelector('#deserts')
 
-entree_btn.addEventListener('click', function(){ HideOthers('recipe-entree'); })
-sides_btn.addEventListener('click', function(){ HideOthers('recipe-side'); })
-breads_btn.addEventListener('click', function(){ HideOthers('recipe-bread'); })
-deserts_btn.addEventListener('click', function(){ HideOthers('recipe-desert'); })
+entree_btn.addEventListener('click', function(){ HideOthers('recipe-group-entree'); })
+sides_btn.addEventListener('click', function(){ HideOthers('recipe-group-side'); })
+breads_btn.addEventListener('click', function(){ HideOthers('recipe-group-bread'); })
+deserts_btn.addEventListener('click', function(){ HideOthers('recipe-group-desert'); })
 all_btn.addEventListener('click', function() { HideNone(); })
 
-function HideOthers(recipeClass){
-    for (let card of cards){
-        // console.log(card.classList)
-        if (!(card.classList.contains(recipeClass))){
-            card.style.display = 'none';
+function HideOthers(recipeGroupId){
+    for (let group of groups){
+        // console.log(group.id)
+        if (!(group.id === recipeGroupId)){
+            group.style.display = 'none';
         }
         else {
-            card.style.display = '';
+            group.style.display = '';
         }
     }
 }
 
 function HideNone (){
-    for (let card of cards){
-        card.style.display = '';
+    for (let group of groups){
+        group.style.display = '';
     }
 }
