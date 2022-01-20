c1b6594cd48a6888f7665f388c9b635de3b2254b
Greg Pendleton <gpendleton@gmail.com>
2022-01-20 13:24:39 -0600

    added spaghetti and cinnamon rolls

--- recipes.js
+++ recipes.js
@@ -1,19 +1,156 @@
 export default[
     {
         id: 1,
-        title: "rolls",
-        ingredients: [
+        title: "cinnamon rolls",
+        category: "desert",
+        ingredientGroups:[
             {
-                ingredient: "cinammom",
-                amount: 1
+                title: "Roll Dough",
+                ingredients: [
+                    {
+                        ingredient: "active dry yeast",
+                        amount: "1 pk"
+                    },
+                    {
+                        ingredient: "warm water",
+                        amount: ".75 tsp"
+                    },
+                    {
+                        ingredient: "milk, scalded",
+                        amount: "1 cup"
+                    },
+                    {
+                        ingredient: "sugar",
+                        amount: ".5 cup"
+                    },
+                    {
+                        ingredient: "shortening",
+                        amount: ".75 cup"
+                    },
+                    {
+                        ingredient: "salt",
+                        amount: "1 tsp"
+                    },
+                    {
+                        ingredient: "all-purpose flour",
+                        amount: "3.5 cups"
+                    },
+                    {
+                        ingredient: "egg, room temp",
+                        amount: 1
+                    }
+                ]
             },
             {
-                ingredient: "butter",
-                amount: 1
+                title: "Filling",
+                ingredients:[
+                    {
+                        ingredient: "cinnamon",
+                        amount: "1 Tbs"
+                    },
+                    {
+                        ingredient: "white sugar",
+                        amount: ".25 cup"
+                    },
+                    {
+                        ingredient: "firmly packed brown sugar",
+                        amount: ".25 cup"
+                    },
+                    {
+                        ingredient: "stick softened butter",
+                        amount: ".25 stick"
+                    }
+                ]
             }
-
         ],
-        directions: ""
+        directions: 
+        [
+            "Makes ½ sheet pan full, 2X for crowd", "Soften yeast in warm water.",
+            "Combine milk, sugar, shortening, and salt; cool to lukewarm.", 
+            "Add 1½ cups of flour; beat well. Beat in yeast and egg. Gradually add remaining flour to form soft dough, beating well.", 
+            "Place in greased bowl, turning once to grease surface.", 
+            "Cover with plastic wrap and let rise till double (1½ - 2 hours)",
+            "Turn out on lightly floured surface and shape as desired - for cinnamon rolls, mix cinnamon and sugars together, roll dough to rectangle approximately ¼ -½” thick, spread butter on rolled out dough, sprinkle cinnamon and sugar mixture evenly.", 
+            "Roll up dough and cut log at 1 inch intervals.", 
+            "Cover and let shaped rolls rise till double (30 to 45 minutes).", 
+            "Bake on greased baking sheet in hot oven (400◦F) for 12 to 15 minutes.", 
+            "Frosting may be added after cooling somewhat - 2 cups confectioner sugar stirred into 2 tablespoons milk, OJ to get desired pouring consistency. Almond or vanilla may be added to taste... Flour quantity may vary according to temp and humidity.", 
+            "Warm = 110-115◦F. Oven = 400◦F"
+        ]
+    },
+    {
+        id: 2,
+        title:"Crock-pot spaghetti",
+        category: "entree",
+        ingredientGroups:[
+            {
+                title: "Main",
+                ingredients:[
+                    {
+                        ingredient: "large green pepper, chopped",
+                        amount: 1
+                    },
+                    {
+                        ingredient: "medium onion, diced",
+                        amount: 1
+                    },
+                    {
+                        ingredient: "clove garlic (or a whole head, your choice 😋)",
+                        amount: 1
+                    },
+                    {
+                        ingredient: "ground beef",
+                        amount: "1.5 lbs"
+                    },
+                    {
+                        ingredient: "salt",
+                        amount: "1 tsp"
+                    },
+                    {
+                        ingredient: "red and/or black pepper to taste",
+                        amount: ""
+                    },
+                    {
+                        ingredient: "spaghetti",
+                        amount: "8 oz"
+                    },
+                    {
+                        ingredient: "grated carrots",
+                        amount: "1 cup"
+                    },
+                    {
+                        ingredient: "tomatoes",
+                        amount: "1 14oz can"
+                    },
+                    {
+                        ingredient: "water",
+                        amount: "20 oz"
+                    },
+                    {
+                        ingredient: "tomato paste",
+                        amount: "1 small can"
+                    },
+                    {
+                        ingredient:"oregano",
+                        amount:"1 Tblspn"
+                    },
+                    {
+                        ingredient: "catsup",
+                        amount:"1 dash"
+                    },
+                    {
+                        ingredient: "Worcestershire sauce",
+                        amount:"1 dash"
+                    }
+                ]
+            }
+        ],
+        directions: [
+        
+            "Brown pepper, onion, and garlic in oil in medium skillet, when onions are translucent, add ground beef. Cook beef and chop into small bits.",
+            "Add remaining ingredients and bring to Boil (\"High\" setting on crock pot), then Simmer - 3 hours",
+            "Add 8 oz spaghetti, Cook on \"Low\" setting of crock pot. Makes lots, can be frozen, do not overcook spaghetti."
+        ]
     }
 ]
 
