cff6f5ff3581d6cfaa8cd0e605f7bde66b501534
Greg Pendleton <gpendleton@gmail.com>
2022-01-19 18:20:36 -0600

    added recipe data file

---
+++ recipes.js
@@ -0,0 +1,19 @@
+export default[
+    {
+        id: 1,
+        title: "rolls",
+        ingredients: [
+            {
+                ingredient: "cinammom",
+                amount: 1
+            },
+            {
+                ingredient: "butter",
+                amount: 1
+            }
+
+        ],
+        directions: ""
+    }
+]
+
